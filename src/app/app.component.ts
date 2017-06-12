a273e166e110335a1240e85e737ca91f232c348f
Ayub Malik <ayub.malik@gmail.com>
2017-06-12 19:23:21 +0100

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector: 'app-root',
+  template: `<h1>Nearest Station Finder</h1>
+  <current-location (change)="updateMessage($event)"></current-location>
+  <div>
+    <p>Current location is {{currentLocation}}</p>
+  </div>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  name = 'NsfApplication';
+
+  currentLocation: string = "?";
+
+  updateMessage(newLocation: string) {
+    this.currentLocation = newLocation;
+  }
+}
---
+++ src/app/current-location.component.ts
@@ -0,0 +1,41 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'current-location',
+  template: `
+    <button (click)="getCurrentLocation()">Get current location</button>
+  `,
+  styles: [`
+    button {
+      outline: 0;
+      cursor: pointer;
+      height: 30px;
+      border: 0;
+      border-radius: 3px;
+      background: #0088cc;
+      color: #fff;
+    }
+  `],
+})
+export class CurrentLocationComponent {
+  name = 'CurrentLocation';
+
+  @Output()
+  change: EventEmitter<string> = new EventEmitter();
+
+  getCurrentLocation() {
+    if (navigator.geolocation) {
+      console.log("aaaa");
+      navigator.geolocation.getCurrentPosition(position => {
+        console.log(position);
+        this.change.emit(position.coords.latitude + "," + position.coords.longitude);
+      }, error2 => {
+        this.change.emit("boom!: " + error2.message);
+      })
+    }
+    else {
+      this.change.emit("geolocation unsupported");
+    }
+  }
+
+}
